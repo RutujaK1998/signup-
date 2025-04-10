0870487ee929445994f27c4b6a1e9b37940e6bc6
Rutuja Kukade <rutujakukade1998@gmail.com>
2025-04-10 22:23:02 +0100

    Fixed path

--- signup/src/App.js
+++ signup/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
-import SignupForm from "./SignupForm";
+import SignupForm from "../src/SignUpForm"
 
 function App() {
   return <SignupForm />;
